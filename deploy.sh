e555486239ec4a287db3e7c3b907d4330deccc53
Johannes Schmidt <johannes.schmidt@immobilienscout24.de>
2015-06-26 16:41:09 +0200

    initial checkin

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE=hello-world-app
+
+mvn package docker:build
+
+docker inspect ${IMAGE} > /dev/null 2>&1
+if [ $? == 0 ]; then
+	docker rm -f ${IMAGE}
+fi
+
+docker run --name ${IMAGE} -p 8080:8080 -t newbiz/hello-world
