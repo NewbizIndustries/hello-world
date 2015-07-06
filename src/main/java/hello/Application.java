5860b401a7bd86664d1e9fb3b782dce9dc07cd4f
Johannes Schmidt <johannes.schmidt@immobilienscout24.de>
2015-07-06 13:36:13 +0200

    new app version v5

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 	@RequestMapping("/")
 	public String home() {
-		return "Hello World // v4";
+		return "Hello World // v5";
 	}
 
 	public static void main(final String[] args) {
